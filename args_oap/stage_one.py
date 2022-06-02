aae472f41daea3d3fbb2248f59e3167ce351c53b
xinehc <xinhec@gmail.com>
2022-06-03 03:29:50 +0800

    add version 3.0 scripts

--- args_oap/stage_one.py
+++ args_oap/stage_one.py
@@ -4,30 +4,21 @@ import os
 def stage_one(options):
 
     _path = os.path.dirname(os.path.realpath(__file__))
-    _stage_one = os.path.join(_path, 'stage_one_version2.3.2')
+    _stage_one = os.path.join(_path, 'argoap_pipeline_stageone_version3')
+
+    args = [
+    _stage_one, 
+    '-i', options.i, 
+    '-o', options.o,
+    '-m', options.m,
+    '-n', str(options.n),
+    '-f', options.f,
+    '-q', str(options.q),
+    '-x', str(options.x),
+    '-y', str(options.y),
+    '-v', str(options.v)]
 
     if options.z:
-        subprocess.call(
-            [_stage_one, '-i', options.i, 
-            '-o', options.o,
-            '-m', options.m,
-            '-n', str(options.n),
-            '-f', options.f,
-            '-q', str(options.q),
-            '-x', str(options.x),
-            '-y', str(options.y),
-            '-v', str(options.v),
-            '-z'])
-    else:
-        subprocess.call(
-            [_stage_one, '-i', options.i, 
-            '-o', options.o,
-            '-m', options.m,
-            '-n', str(options.n),
-            '-f', options.f,
-            '-q', str(options.q),
-            '-x', str(options.x),
-            '-y', str(options.y),
-            '-v', str(options.v)
-            ])
-       
+        args.extend(['-z'])
+
+    subprocess.call(args)
