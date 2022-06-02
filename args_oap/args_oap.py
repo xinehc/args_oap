6c696726d6d00d63c1fcf6e8b66856071468e9b9
xinehc <xinehc@gmail.com>
2022-06-03 03:29:50 +0800

    add version 3.0 scripts

---
+++ args_oap/args_oap.py
@@ -0,0 +1,76 @@
+import sys
+import os
+import subprocess
+
+from argparse import ArgumentParser
+from .stage_one import stage_one
+from .stage_two import stage_two
+
+workingdir = os.path.abspath(os.path.dirname(__file__))
+def parse_options(argv):
+
+    parser = ArgumentParser(prog='args_oap')
+    subparsers = parser.add_subparsers(help='Running modes', metavar='{stage_one, stage_two}')
+
+    ## stage_one parameters
+    parser_stage_one = subparsers.add_parser('stage_one', help='run stage_one')
+    mandatory_one = parser_stage_one.add_argument_group('MANDATORY')
+    mandatory_one.add_argument('-i', help='input files directory', metavar='DIR', required=True)
+    mandatory_one.add_argument('-o', help='output files directory', metavar='DIR', required=True)
+    mandatory_one.add_argument('-m', help='meta data file', metavar='FILE', required=True)
+
+    optional_one = parser_stage_one.add_argument_group('OPTIONAL')
+    optional_one.add_argument('-n', help='number of threads, default 1', default=1)
+    optional_one.add_argument('-f', help='the format of processed files, default fq', default='fq')
+    optional_one.add_argument('-q', help='quality control of fastq sequences defualt not take effect, set to 1, then will do QC with fastp', default=0)
+    optional_one.add_argument('-z', help='whether the fq files were .gz format, if -z, then firstly gzip -d, default(none) ', default=False, action='store_true')
+    optional_one.add_argument('-x', help='evalue for searching 16S in usearch default 1e-10', default=1e-10)
+    optional_one.add_argument('-y', help='evalue for searching universal single copy marker gene default 3', default=3)
+    optional_one.add_argument('-v', help='the identity value for diamond to search the USCMGs default  0.45', default=0.45)
+
+    parser_stage_one.set_defaults(func=stage_one)
+
+    ## stage_one parameters
+    parser_stage_two = subparsers.add_parser('stage_two', help='run stage_two')
+    mandatory_two = parser_stage_two.add_argument_group('MANDATORY')
+    mandatory_two.add_argument('-i', help='the potential arg reads from stage one', required=True)
+    mandatory_two.add_argument('-o', help='Output prefix', required=True)
+    mandatory_two.add_argument('-m', help='meta data online from stage one', required=True)
+
+    optional_two = parser_stage_two.add_argument_group('OPTIONAL')
+    optional_two.add_argument('-n', help='lnumber of threads used for blastx, default 1', default=1)
+    optional_two.add_argument('-b', help='if set then process the blastx results directly [default off], useful if user want to accelerate the stage two by running blastx paralell', default=False)
+    optional_two.add_argument('-l', help='length filtering default 75 percent', default=75)
+    optional_two.add_argument('-e', help='evalue cutoff, default 1e-7', default=1e-7)
+    optional_two.add_argument('-d', help='identity cutoff, default 80', default=80)
+
+    optional_two.add_argument('-db', help='reference ARG database which is blastxable after makeblastdb, default is SARG database', default=workingdir+"/DB/SARG_20210519.fasta")
+    optional_two.add_argument('-fa', help='reference ARG database in fasta format, default is SARG database', default=workingdir+"/DB/SARG_20210519.fasta")
+    optional_two.add_argument('-struc1', help='reference ARG database structure, default is SARG database', default=workingdir+"/DB/structure_20210519_for_pipeline.txt")
+    optional_two.add_argument('-struc2', help='reference ARG database structure, default is SARG database', default=workingdir+"/DB/multi_component.txt")
+    optional_two.add_argument('-struc3', help='reference ARG database structure, default is SARG database', default=workingdir+"/DB/two_component.txt")
+
+    parser_stage_two.set_defaults(func=stage_two)
+
+    if len(argv) < 2:
+        parser.print_help()
+        sys.exit(2)
+
+    if len(argv) < 3:
+        if argv[1] == 'stage_one':
+            parser_stage_one.print_help()
+        elif argv[1] == 'stage_two':
+            parser_stage_two.print_help()
+        else:
+            parser.print_help()
+        sys.exit(2)
+
+    return(parser.parse_args(argv[1:]))
+
+def main(argv=sys.argv):
+
+    options = parse_options(argv)
+    options.func(options)
+
+if __name__ == "__main__":
+    main(sys.argv)
--- args_oap/stage_two.py
+++ args_oap/stage_two.py
@@ -4,24 +4,25 @@ import os
 def stage_two(options):
 
     _path = os.path.dirname(os.path.realpath(__file__))
-    _stage_two = os.path.join(_path, 'stage_two_version2')
+    _stage_two = os.path.join(_path, 'argoap_pipeline_stagetwo_version3.py')
+
+    args = [
+    'python',
+    _stage_two, 
+    '-i', options.i, 
+    '-o', options.o,
+    '-m', options.m,
+    '-n', str(options.n),
+    '-l', str(options.l),
+    '-e', str(options.e),
+    '-d', str(options.d),
+    '-db',options.db,
+    '-fa',options.fa,
+    '-struc1',options.struc1,
+    '-struc2',options.struc2,
+    '-struc3',options.struc3]
 
     if options.b:
-        subprocess.call(
-            [_stage_two, '-i', options.i, 
-            '-o', options.o,
-            '-m', options.m,
-            '-n', str(options.n),
-            '-l', str(options.l),
-            '-e', str(options.e),
-            '-d', str(options.d),
-            '-b'])
-    else:
-        subprocess.call(
-            [_stage_two, '-i', options.i, 
-            '-o', options.o,
-            '-m', options.m,
-            '-n', str(options.n),
-            '-l', str(options.l),
-            '-e', str(options.e),
-            '-d', str(options.d)])
+        args.extend(['-b', str(options.b)])
+
+    subprocess.call(args)
