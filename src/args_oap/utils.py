ad82c51a53c98af11572b4655c58174784e871ca
xinehc <xinehc@gmail.com>
2022-10-16 20:11:17 +0800

    print stderr when make_db fails

--- src/args_oap/utils.py
+++ src/args_oap/utils.py
@@ -57,7 +57,7 @@ def simple_count(file):
 
 def make_db(file):
     '''
-    Make database. The type of the database will be determined based on diamond'd return code.
+    Make database. The type of the database will be determined based on diamond's return code.
     '''
     if not os.path.isfile(file):
         logger.critical('File <{}> cannot be found. Please check input file (-i/--infile).'.format(file))
@@ -66,17 +66,17 @@ def make_db(file):
     logger.info('Building database of <{}> ...'.format(file))
     
     ## diamond
-    subp = subprocess.run(['diamond', 'makedb', '--in', file, '--db', file, '--quiet'], check=False, stderr=subprocess.DEVNULL)
+    subp = subprocess.run(['diamond', 'makedb', '--in', file, '--db', file, '--quiet'], check=False, stderr=subprocess.PIPE)
     
     if subp.returncode == 1: # auto choose dbtype by checking diamond's return code
-        subprocess.run(['bwa', 'index', file], check=True, stderr=subprocess.DEVNULL)
-        dbtype = 'nucl'
-    else:
-        if os.path.isfile(file + '.dmnd'):
-            dbtype = 'prot'
+        if subp.stderr == b'Error: The sequences are expected to be proteins but only contain DNA letters. Use the option --ignore-warnings to proceed.\n':
+            subprocess.run(['bwa', 'index', file], check=True, stderr=subprocess.DEVNULL)
+            dbtype = 'nucl'
         else:
-            logger.critical('Cannot build diamond database of <{}>'.format(file))
+            logger.critical('Cannot build diamond database of <{}> ({}). Please check input file (-i/--infile).'.format(file, subp.stderr))
             sys.exit(2)
+    else:
+        dbtype = 'prot'
 
     subprocess.run(['makeblastdb', '-in', file, '-dbtype', dbtype, '-out', file], check=True, stdout=subprocess.DEVNULL)
     logger.info('Finished.')
